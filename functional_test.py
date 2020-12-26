cbeaa7deffeea9b947ebe4e07efa80f0acbc7366
Eduardo Roessler <edu.roessler@gmail.com>
2020-12-25 22:21:33 -0300

    I dont know

--- functional_test.py
+++ functional_test.py
@@ -38,18 +38,16 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_elements_by_id('id_list_table')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
-        # Ainda continua havendo uma caixa de texto convidando-a a acrescentar
-        # outro item. Ela insere "Use peacock feathers to make a fly"
-        # (Usar penas de pavão para fazer um fly - Edith é bem metódica)
+        # Edith se pergunta se o site lembrará de sua lista. Então ela nota
+        # que o site gerou um URL único para ela -- há um pequeno
+        # texto aplicativo para isso.
         self.fail('Finish the test!')
-
-        # A página é atualizada novamente e agora mostra os dois itens em sua lista
-
+        
+        # Ela acessa essa URL - sua lista de tarefas continua lá.
 if __name__ == '__main__':
     unittest.main(warnings='ignore')                  
